83a2cf5458db97352edcbbb4afc4bdf2d2494610
Tiendz6 <128158864+Tiendz6@users.noreply.github.com>
2023-06-01 05:46:44 +0700

    12

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,18 +1,66 @@
 function signup(){
   event.preventDefault();
-  var username=document.getElementById("username").value;
-  var password=document.getElementById("password").value;
-  var coin=document.getElementById("coin").value;
-  var user={
-    username:username,
-    password:password,
-    coin:coin,
-  }
-  var json= JSON.stringify(user);
-  localStorage.setItem(username,json);
-  alert("ĐĂNG KÝ THÀNH CÔNG");
-  window.location.href="account.html"
+  let username=document.getElementById("username").value;
+  let password=document.getElementById("password").value;
+  let coin=document.getElementById("coin").value;
+  let list = localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+  list.push({
+    username: username,
+    password: password,
+    coin: coin,
+  });
+  localStorage.setItem("list", JSON.stringify(list));
+  renderAccount();
 }
+
+/*Hiển thị tài khoản*/
+function renderAccount() {
+  let list = localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+  let Account = "";
+  list.map((value, index) => {
+    Account +=
+    '<div class="account-container"><div class="STT">' + (index + 1) +
+    '</div><div class="Thao_tac"><button style="color:red;" onclick="xoa(' +
+    index +
+    ')">Xóa</button></div><div class="Tai_khoan">' +
+    value.username +
+    '</div><div class="Mat_khau">' +
+    value.password +
+    '</div><div class ="So_tien">' +
+    value.coin;
+    '</div></div>'
+  });
+  document.querySelector(".account-container").innerHTML = Account;
+  sum();
+  // document.querySelector("#so").innerHTML = list.length;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*Bấm nút add account*/
 const toggleaddbutton = document.querySelector('.add-button')
